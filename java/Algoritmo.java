b7b2946690d7db72f4864a3ebd49f0c61f5939f8
Magui834 <mmagui834@gmail.com>
2021-10-18 21:32:37 -0500

    update: Hola

---
+++ java/Algoritmo.java
@@ -0,0 +1,26 @@
+/**
+* @author: Martinez Meza Margarita
+* @email: mmagui834@gmail.com
+* @about: Clase para implementar Dijkstra's algorithm
+*/
+class AlgoritmoDJ{
+
+    /** 
+    *En este metodo se iniciara sel setup del agoritmo
+     */
+     public void init(){ 
+         this.start();
+     }
+    /**En este metodo se inicia el algoritmo
+     */
+    private void start(){
+        // . . .
+        // . . .
+        // . . . 
+    Syste,.out.print("La tierra les dice Holaaa");
+    }
+    public static void main(String[] args){
+     AlgoritmoDJ algoritmoDJ=news AlgoritmoDJ();
+     algoritmoDJ.init();
+    }
+}
